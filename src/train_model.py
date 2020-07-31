d830562c54741a5cde9c678dd6f2db3c3de3e0a4
saratbhargava <sarat2895@gmail.com>
2020-07-30 18:36:15 -0700

    Fix train dense two layer net and hparams

--- src/train_model.py
+++ src/train_model.py
@@ -41,66 +41,65 @@ def get_logistic_regression(preprocessing_layer):
     return model
 
 
-def train_dense_two_layer_net_hparam_helper(
-        preprocessing_layer, hparams,
-        HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER, HP_EPOCHS,
-        logs, models_dir):
-    print(hparams, hparams[HP_DROPOUT], hparams[HP_NUM_UNITS])
-    model = Sequential(
-        [
-            preprocessing_layer,
-            Dense(hparams[HP_NUM_UNITS], activation='relu'),
-            Dropout(hparams[HP_DROPOUT]),
-            Dense(hparams[HP_NUM_UNITS], activation='relu'),
-            Dense(1, activation='sigmoid')
-        ]
-    )
-    model.compile(
-        optimizer=RMSprop(lr=hparams[HP_OPTIMIZER]),
-        loss=BinaryCrossentropy(),
-        metrics=['accuracy', 'AUC'])
-    model_checkpoint = ModelCheckpoint(
-        # _{epoch:02d}_{val_accuracy:.02f}
-        filepath=f'{models_dir}/checkpoint',
-        save_best_only=True, verbose=1, monitor='val_accuracy')
-    log_dir = logs + datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(
-        log_dir=log_dir, histogram_freq=1)
-    hp_callback = hp.KerasCallback(logs, hparams)
-    callbacks = [model_checkpoint, tensorboard_callback,
-                 hp_callback]
-    history = model.fit(train_data, validation_data=valid_data,
-                        epochs=hparams[HP_EPOCHS], callbacks=callbacks)
-    with tf.summary.create_file_writer(logs).as_default():
-        hp.hparams(hparams)
-        tf.summary.scalar(
-            METRIC_ACCURACY, history.history['val_accuracy'][-1], step=1)
-    model.save(f"{models_dir}/final_epoch_{history.history['val_accuracy'][-1]}_" +
-               datetime.now().strftime("%Y%m%d-%H%M%S"))
+class train_dense_two_layer_net_hparam:
+    # A bad way to use classes, can use funcs
 
+    def __init__(self, preprocessing_layer, HP_NUM_UNITS, HP_DROPOUT,
+                 HP_OPTIMIZER, HP_EPOCHS, exp_name='exp1'):
 
-def train_dense_two_layer_net_hparam(
-        preprocessing_layer, HP_NUM_UNITS, HP_DROPOUT,
-        HP_OPTIMIZER, HP_EPOCHS, exp_name='exp1'):
+        self.HP_NUM_UNITS = HP_NUM_UNITS
+        self.HP_DROPOUT = HP_DROPOUT
+        self.HP_OPTIMIZER = HP_OPTIMIZER
+        self.HP_EPOCHS = HP_EPOCHS
 
-    session_num = 0
-    for num_units in HP_NUM_UNITS.domain.values:
-        for dropout_rate in (HP_DROPOUT.domain.min_value, HP_DROPOUT.domain.max_value):
-            for optimizer in HP_OPTIMIZER.domain.values:
-                for epochs in HP_EPOCHS.domain.values:
-                    hparams = {
-                        HP_NUM_UNITS: num_units,
-                        HP_DROPOUT: dropout_rate,
-                        HP_OPTIMIZER: optimizer,
-                        HP_EPOCHS: epochs
-                    }
-                    session_name = f'num_units_{num_units}-dropout_{dropout_rate}-lr_{optimizer}-epochs_{epochs}'
-                    print(f'Session number: {session_num}')
-                    print({h.name: hparams[h] for h in hparams})
-                    train_dense_two_layer_net_hparam_helper(
-                        preprocessing_layer, hparams,
-                        HP_NUM_UNITS, HP_DROPOUT,
-                        HP_OPTIMIZER, HP_EPOCHS,
-                        f'logs/{exp_name}/' + session_name,
-                        f'models/{exp_name}/' + session_name)
-                    session_num += 1
+        session_num = 0
+        for num_units in HP_NUM_UNITS.domain.values:
+            for dropout_rate in (HP_DROPOUT.domain.min_value, HP_DROPOUT.domain.max_value):
+                for optimizer in HP_OPTIMIZER.domain.values:
+                    for epochs in HP_EPOCHS.domain.values:
+                        hparams = {
+                            HP_NUM_UNITS: num_units,
+                            HP_DROPOUT: dropout_rate,
+                            HP_OPTIMIZER: optimizer,
+                            HP_EPOCHS: epochs
+                        }
+                        session_name = f'num_units_{num_units}-dropout_{dropout_rate}-lr_{optimizer}-epochs_{epochs}'
+                        print(f'Session number: {session_num}')
+                        print({h.name: hparams[h] for h in hparams})
+                        self.__helper__(
+                            hparams,
+                            f'logs/{exp_name}/' + session_name,
+                            f'models/{exp_name}/' + session_name)
+                        session_num += 1
+
+    def __helper__(self, hparams, logs, models_dir):
+        model = Sequential(
+            [
+                preprocessing_layer,
+                Dense(hparams[self.HP_NUM_UNITS], activation='relu'),
+                Dropout(hparams[self.HP_DROPOUT]),
+                Dense(hparams[self.HP_NUM_UNITS], activation='relu'),
+                Dense(1, activation='sigmoid')
+            ]
+        )
+        model.compile(
+            optimizer=RMSprop(lr=hparams[self.HP_OPTIMIZER]),
+            loss=BinaryCrossentropy(),
+            metrics=['accuracy', 'AUC'])
+        model_checkpoint = ModelCheckpoint(
+            filepath=f'{models_dir}/checkpoint',
+            save_best_only=True, verbose=1, monitor='val_accuracy')
+        log_dir = logs + datetime.now().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir=log_dir, histogram_freq=1)
+        hp_callback = hp.KerasCallback(logs, hparams)
+        callbacks = [model_checkpoint, tensorboard_callback,
+                     hp_callback]
+        history = model.fit(train_data, validation_data=valid_data,
+                            epochs=hparams[self.HP_EPOCHS], callbacks=callbacks)
+        with tf.summary.create_file_writer(logs).as_default():
+            hp.hparams(hparams)
+            tf.summary.scalar(
+                METRIC_ACCURACY, history.history['val_accuracy'][-1], step=1)
+        model.save(f"{models_dir}/final_epoch_{history.history['val_accuracy'][-1]}_" +
+                   datetime.now().strftime("%Y%m%d-%H%M%S"))
